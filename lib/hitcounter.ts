36a8601fb6632bac3c871f27ebae99f72719a83e
Eric Cassells <Castro-711@users.noreply.github.com>
2023-05-11 12:13:33 +0100

    Define resources in the HitCounter Stack

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import { Construct } from 'constructs';
 
 export interface HitCounterProps {
@@ -8,10 +9,6 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
-    constructor(scope: Construct, id: string, props: HitCounterProps) {
-        super(scope, id)
-    }
-
     /**
      * Whats goin on here? 
      *  - We delcared a new construct called HitCounter
@@ -21,4 +18,40 @@ export class HitCounter extends Construct {
      */
 
     // TODO
+
+    // now let's add our Lambda & Dynamo Resources to our HitCounter Construct
+
+    // allows accessing the counter function
+    public readonly handler: lambda.Function;
+
+    constructor(scope: Construct, id: string, props: HitCounterProps) {
+        super(scope, id)
+
+        const table = new dynamodb.Table(this, 'Hits', {
+            partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+        })
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        })
+    }
+
+    /**
+     * What did we do here?
+     *  - defined a DynamoDB table with path as the partition key
+     *  - defined a Lambda func which is bound to the lambda/hitcounter.handler code
+     *  - wired the Lambdas env vars to the functionName & tableName of our resources
+     * 
+     * Late-bound values
+     *  The functionName & tableName properties are values that only resolve when we deploy our stack (notice that we haven't configured these physical names when we 
+     *      defined the table/function only logical IDs). This means that if you print their values during syntheses, you will get a "TOKEN", which is how the CDK represents these 
+     *      late-bound values. You should treat TOKENS as opaque strings. 
+     *      This means you can concatenate them together for example, but don't be tempted to parse them in your code !!!!
+     */
 }
